7b536a8644a9e73f52717b08aab5ddd2aceab7fc
Tianyi Li <Cinccino@users.noreply.github.com>
2016-12-19 18:59:29 +0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python
 ## -*- coding: utf-8 -*-
-##
-##  Jonathan Salwan - 2014-05-12 - ROPgadget tool
-##
-##  http://twitter.com/JonathanSalwan
-##  http://shell-storm.org/project/ROPgadget/
-##
 
 from capstone   import *
 from ctypes     import *
